32af38d64a4818b19604a556aaf2e628175848e5
vikashr <vikashr@sfu.ca>
2021-02-21 23:07:59 -0800

    work in progress

--- test.js
+++ test.js
@@ -3,32 +3,25 @@ let fileUtils = require("./csvToJson/src/util/fileUtils");
 var fs = require('fs');
 
 
-function bar (err, data) {
-  /* If an error exists, show it, otherwise show the file */
-  err ? Function("error","throw error")(err) : JSON.stringify(data);
-};
-
-
-
 // Testcase #1
 console.log('\n>>>>> Starting test case 1');
 
 let input1 = './TestData/TestFiles/test1Input.csv';
 let output1 = './TestOutput/Files/test1Output.json';
 
-
 csvToJson.generateJsonFileFromCsv(input1, output1);
 
 
-// let test1Result = fileUtils.readFile(output1);
-//
-// let test1Output = fileUtils.readFile('./TestData/ExpectedOutput/test1Output.json');
+var test1Result = fileUtils.readFile('./TestOutput/Files/test1Output.json', this.encoding);
 
-let test1Result = fs.readFile(output1, bar);
-let test1Output = fs.readFile('./TestData/ExpectedOutput/test1Output.json', bar);
+var test1Output = fileUtils.readFile('./TestData/ExpectedOutput/test1Output.json', this.encoding);
 
 
 if (test1Result === test1Output)
     console.log('>>>>> Test 1 Passed');
 else
     console.log('>>>>> Test 1 Failed');
+
+console.log(test1Result);
+console.log("next");
+console.log(test1Output);
