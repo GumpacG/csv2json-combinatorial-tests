0620014dde3b68fdb06a7b8486dce8fc5de48ac6
Guian Gumpac <ggumpac@gmail.com>
2021-02-22 23:44:34 -0800

    test 1

--- csvToJson/test/csvToJsonTest.js
+++ csvToJson/test/csvToJsonTest.js
@@ -8,15 +8,15 @@ let fs = require('fs');
 describe('csvToJson test', function () {
     it('test 1', function() {
         let input1 = 'test/TestData/TestFiles/test1Input.csv';
-        let output1 = 'test/TestOutput/Files/test1Output.json';
-        let expectedOutput = require('./TestData/ExpectedOutput/test1Output.json');
+        let output1 = 'test/TestOutput/Files/test1Output.txt';
         
         let result = index.getJsonFromCsv(input1);
-
         fs.writeFile(output1, JSON.stringify(result), function (err) {
             if (err) throw err;
             });
 
-        expect(result).to.deep.equal(expectedOutput);
+        fs.readFile('test/TestData/ExpectedOutput/test1Output.txt', 'utf8', function(err, data){ 
+            expect(JSON.stringify(result)).to.deep.equal(data);
+        }); 
     });
 });
