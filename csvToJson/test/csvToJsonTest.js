bd5dd33ab94dc363a0a3f5cf477a85d682ee6382
Guian Gumpac <ggumpac@gmail.com>
2021-02-23 07:58:30 -0800

    fixed the first test

--- csvToJson/test/csvToJsonTest.js
+++ csvToJson/test/csvToJsonTest.js
@@ -2,21 +2,20 @@
 
 let expect = require('chai').expect;
 let assert = require('chai').assert;
-let index = require('../index');
+let csvToJson = require('../src/csvToJson');
 let fs = require('fs');
 
 describe('csvToJson test', function () {
     it('test 1', function() {
         let input1 = 'test/TestData/TestFiles/test1Input.csv';
         let output1 = 'test/TestOutput/Files/test1Output.txt';
-        
-        let result = index.getJsonFromCsv(input1);
-        fs.writeFile(output1, JSON.stringify(result), function (err) {
-            if (err) throw err;
-            });
 
-        fs.readFile('test/TestData/ExpectedOutput/test1Output.txt', 'utf8', function(err, data){ 
-            expect(JSON.stringify(result)).to.deep.equal(data);
+        csvToJson.generateJsonFileFromCsv(input1, output1);        
+
+        fs.readFile('test/TestData/ExpectedOutput/test1Output.txt', 'utf8', function(err, expectedOutput){ 
+            fs.readFile(output1, 'utf8', function(err, actualOutput){ 
+                expect(expectedOutput).to.deep.equal(actualOutput);
+            }); 
         }); 
     });
 });
