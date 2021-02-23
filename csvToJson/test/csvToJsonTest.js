9e55a8d06330499831d931a1aa660f2ee0a26048
Guian Gumpac <ggumpac@gmail.com>
2021-02-23 08:13:40 -0800

    added second test

--- csvToJson/test/csvToJsonTest.js
+++ csvToJson/test/csvToJsonTest.js
@@ -7,14 +7,29 @@ let fs = require('fs');
 
 describe('csvToJson test', function () {
     it('test 1', function() {
-        let input1 = 'test/TestData/TestFiles/test1Input.csv';
-        let output1 = 'test/TestOutput/Files/test1Output.txt';
+        let input = 'test/TestData/TestFiles/test1Input.csv';
+        let output = 'test/TestOutput/Files/test1Output.txt';
+        let expectedOutput = 'test/TestData/ExpectedOutput/test1Output.txt';
 
-        csvToJson.generateJsonFileFromCsv(input1, output1);        
+        csvToJson.generateJsonFileFromCsv(input, output);        
 
-        fs.readFile('test/TestData/ExpectedOutput/test1Output.txt', 'utf8', function(err, expectedOutput){ 
-            fs.readFile(output1, 'utf8', function(err, actualOutput){ 
-                expect(expectedOutput).to.deep.equal(actualOutput);
+        fs.readFile(expectedOutput, 'utf8', function(err, expectedResult){ 
+            fs.readFile(output, 'utf8', function(err, actualResult){ 
+                expect(expectedResult).to.deep.equal(actualResult);
+            }); 
+        }); 
+    });
+
+    it('test 2', function() {
+        let input = 'test/TestData/TestFiles/test2Input.txt';
+        let output = 'test/TestOutput/Files/.json';
+        let expectedOutput = 'test/TestData/ExpectedOutput/test2Output.json';
+
+        csvToJson.generateJsonFileFromCsv(input, output);        
+
+        fs.readFile(expectedOutput, 'utf8', function(err, expectedResult){ 
+            fs.readFile(output, 'utf8', function(err, actualResult){ 
+                expect(expectedResult).to.deep.equal(actualResult);
             }); 
         }); 
     });
