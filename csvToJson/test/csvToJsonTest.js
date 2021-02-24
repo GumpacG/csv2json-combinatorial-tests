5d5ecf99b6e10ba55466493f72a5859e76ee321f
Vikash Reddy <vikash_reddy@hotmail.com>
2021-02-23 21:30:15 -0800

    test 11 to 14

--- csvToJson/test/csvToJsonTest.js
+++ csvToJson/test/csvToJsonTest.js
@@ -2,21 +2,64 @@
 
 let expect = require('chai').expect;
 let assert = require('chai').assert;
-let index = require('../index');
+let csvToJson = require('../src/csvToJson');
 let fs = require('fs');
 
 describe('csvToJson test', function () {
-    it('test 1', function() {
-        let input1 = 'test/TestData/TestFiles/test1Input.csv';
-        let output1 = 'test/TestOutput/Files/test1Output.json';
-        let expectedOutput = require('./TestData/ExpectedOutput/test1Output.json');
+    it('test 11', function() {
+        let input = 'test/TestData/TestFiles/test11Input.csv';
+        let output = 'test/TestOutput/Files/test11Output.json';
+        let expectedOutput = 'test/TestData/ExpectedOutput/test11Output.json';
 
-        let result = index.getJsonFromCsv(input1);
+        csvToJson.generateJsonFileFromCsv(input, output);
 
-        fs.writeFile(output1, JSON.stringify(result), function (err) {
-            if (err) throw err;
+        fs.readFile(expectedOutput, 'utf8', function(err, expectedResult){
+            fs.readFile(output, 'utf8', function(err, actualResult){
+                expect(actualResult).to.deep.equal(expectedResult);
             });
-
-        expect(result).to.deep.equal(expectedOutput);
+        });
     });
+
+    it('test 12', function() {
+        let input = 'test/TestData/TestFiles/test12Input.txt';
+        let output = 'test/TestOutput/Files/test12Output.txt';
+        let expectedOutput = 'test/TestData/ExpectedOutput/test12Output.txt';
+
+        csvToJson.generateJsonFileFromCsv(input, output);
+
+        fs.readFile(expectedOutput, 'utf8', function(err, expectedResult){
+            fs.readFile(output, 'utf8', function(err, actualResult){
+                expect(actualResult).to.deep.equal(expectedResult);
+            });
+        });
+    });
+
+    it('test 13', function() {
+        let input = 'test/TestData/TestFiles/test13Input.txt';
+        let output = 'test/TestOutput/Files/test13Output.txt';
+        let expectedOutput = 'test/TestData/ExpectedOutput/test13Output.txt';
+
+        csvToJson.generateJsonFileFromCsv(input, output);
+
+        fs.readFile(expectedOutput, 'utf8', function(err, expectedResult){
+            fs.readFile(output, 'utf8', function(err, actualResult){
+                expect(actualResult).to.deep.equal(expectedResult);
+            });
+        });
+    });
+
+    it('test 14', function() {
+        let input = 'test/TestData/TestFiles/test14Input.csv';
+        let output = 'test/TestOutput/Files/test14Output.txt';
+        let expectedOutput = 'test/TestData/ExpectedOutput/test14Output.txt';
+
+        csvToJson.generateJsonFileFromCsv(input, output);
+
+        fs.readFile(expectedOutput, 'utf8', function(err, expectedResult){
+            fs.readFile(output, 'utf8', function(err, actualResult){
+                expect(actualResult).to.deep.equal(expectedResult);
+            });
+        });
+    });
+
 });
